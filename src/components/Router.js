2c58d8e4e874b00d70ce3861212033283894e6f1
animasana <animagig@gmail.com>
2024-02-22 19:28:34 +0900

    -

--- src/components/Router.js
+++ src/components/Router.js
@@ -6,11 +6,10 @@ import {
 
 import Home from '../routes/Home';
 import Detail from '../routes/Detail';
-import { REPOSITORY } from '../constants';
 
 export default function Router() {
   return (
-    <BRouter basename={`${process.env.PUBLIC_URL}${REPOSITORY}`}>
+    <BRouter basename={`${process.env.PUBLIC_URL}`}>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/movie/:id' element={<Detail />} />
