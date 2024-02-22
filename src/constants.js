0dd625bcd7d0e4fc61c5103f5fa485e692589bb4
animasana <animagig@gmail.com>
2024-02-22 20:36:43 +0900

    +css

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,4 @@
-export const API_URL = `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.3&sort_by=year&limit=50`;
+export const API_URL = `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.3&with_rt_ratings=true&sort_by=year&limit=50`;
 export function getMovieURL(id) {
   return `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`;
 }
-
-export const REPOSITORY = '/react-for-beginners-202402';
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { API_URL } from "../constants";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
@@ -15,18 +16,18 @@ export default function Home() {
   useEffect(() => {    
     getMovies();
   }, []);
-
-  console.log(movies);
+  
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map(movie => 
             <Movie 
               key={movie.id}
               id={movie.id}
+              year={movie.year}
               coverImg={movie.medium_cover_image} 
               title={movie.title}
               summary={movie.summary}
